12a92672e4baed034784d66c44fdee74b2a7d3ab
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-02 21:12:12 +0200

    Create basic home page for notes topics

--- firstwebsite/notes/views.py
+++ firstwebsite/notes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, Note, Comment
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Notes test")
+    latest_topics_list = Topic.objects.order_by("-updated_at")
+    context = {"latest_topics_list": latest_topics_list}
+    return render(request, "notes/index.html", context)
