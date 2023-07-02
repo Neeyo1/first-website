9daef2d4ddf415685eb6fc3ee781705674f3ed1c
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-02 16:50:41 +0200

    Register database object on admin page

--- firstwebsite/notes/admin.py
+++ firstwebsite/notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Topic, Note, Comment
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Note)
+admin.site.register(Comment)
