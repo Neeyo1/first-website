2bbbfea4334af3164fe62a7e7e41421efa769def
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-02 16:43:20 +0200

    Create database objects

--- firstwebsite/notes/models.py
+++ firstwebsite/notes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Note(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField()
+    completed_by = User()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    message = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
